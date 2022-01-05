5cf8aede4e50f7d198d530d99c49a1fff6672d29
Rose Adam <rosedarline@gmail.com>
2022-01-05 17:23:13 -0500

    Add route and render server at TextEditor

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -2,7 +2,7 @@ import React, { useCallback, useEffect, useState } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 import { io } from "socket.io-client";
-
+import { useParams } from "react-router-dom";
 
 const TOOLBAR_OPTIONS = [
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -17,8 +17,10 @@ const TOOLBAR_OPTIONS = [
 ]
 
 export default function TextEditor() {
+    const { id: documentId } = useParams()
     const [socket, setSocket] = useState()
     const [quill, setQuill] = useState()
+    console.log(documentId)
 
     useEffect(() => {
         const s = io("http://localhost:3001")
@@ -28,6 +30,17 @@ export default function TextEditor() {
             s.disconnect()
         }
     }, [])
+
+    useEffect(() => {
+        if (socket == null || quill == null) return
+
+        socket.once("load-document", document => {
+            quill.setContents(document)
+            quill.enable()
+        })
+
+        socket.emit("get-document", documentId)
+    }, [socket, quill, documentId])
 
     useEffect(() => {
         if (socket == null || quill == null) return
@@ -70,6 +83,8 @@ export default function TextEditor() {
         const q = new Quill(editor, {
             theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS },
         })
+        quill.disable()
+        q.setText("Loading...")
         setQuill(q)
 
     }, [])
